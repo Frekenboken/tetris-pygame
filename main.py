a2f75f31cc3100d864885552d923bed07160faec
xiaomi <pavel.ov07@mail.ru>
2025-02-14 13:23:00 +0300

    update

--- main.py
+++ main.py
@@ -1,100 +1,162 @@
-import os
 import random
-import sys
 import time
+import csv
 
-import numpy as np
 import pygame
 from pygame import Surface
 
-from pygame_utils import terminate, load_level, load_image
+from pygame_utils import terminate, load_image
 from utils import rotate_matrix
-from figures import tetris_shapes
+from shapes import tetris_shapes
 import colors
 
 field_size = field_width, field_height = (10, 20)
-cell_size = 30
+CELL_SIZE = 30
 SIZE = WIDTH, HEIGHT = (1000, 600)
 FPS = 50
+
+RECORDS_PATH = 'data/high_scores.csv'
+PIXEL_FONT_PATH = 'data/Jersey10-Regular.ttf'
+START_IMG_PATH = 'start.png'
+ANIMATION_PATH = 'anim.png'
+
 pygame.init()
 pygame.display.set_caption("TETRIS")
 screen = pygame.display.set_mode(SIZE)
 
+# Звуковые эффекты
+game_over_sound = pygame.mixer.Sound('data/game_over.wav')
+
 player = None
+animation_sprites = pygame.sprite.Group()
+
+
+def records_add(new_record):
+    with open(RECORDS_PATH, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        records = [int(row[0]) for row in reader]
+
+    if len(records) < 3 or new_record > min(records):
+        records.append(new_record)
+        records = sorted(records, reverse=True)[:3]
+        with open(RECORDS_PATH, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows([[record] for record in records])
+
+
+def records_read():
+    try:
+        with open(RECORDS_PATH, mode='r', newline='') as file:
+            reader = csv.reader(file)
+            records = [int(row[0]) for row in reader]
+        return sorted(records, reverse=True)[:3]
+    except FileNotFoundError:
+        return []
 
 
 def game_over_screen(score):
-    intro_text = ["ИГРА ОКОНЧЕНА", "",
-                  f"Ваш счет: {score}",
-                  "Нажмите R для перезапуска",
-                  "Нажмите Q для выхода"]
-
-    # fon = pygame.transform.scale(load_image('Default.jpg'), (WIDTH, HEIGHT))
-    fon = pygame.surface.Surface(WIDTH, HEIGHT)
-    fon.fill(colors.BLUE)
+    game_over_sound.play()
+    fon = pygame.surface.Surface((WIDTH, HEIGHT))
+    fon.fill(colors.BLACK)
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
 
+    font = pygame.font.Font(PIXEL_FONT_PATH, 60)
+    font_mini = pygame.font.Font(PIXEL_FONT_PATH, 30)
+
+    score_string = font.render("Your score: " + str(score), 1, colors.WHITE)
+    screen.blit(score_string, (WIDTH / 2 - score_string.get_width() / 2,
+                               HEIGHT / 2 - score_string.get_height() / 2))
+    timer = time.time()
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    return True  # Перезапуск игры
-                elif event.key == pygame.K_q:
-                    terminate()  # Выход из игры
+        if time.time() - timer > 2.4:
+
+            press_string = font_mini.render("Press any button", 1, colors.WHITE)
+            screen.blit(press_string,
+                        (WIDTH / 2 - press_string.get_width() / 2,
+                         HEIGHT / 2 - press_string.get_height() / 2 + 70))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN:
+                    return True
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    return True
+        else:
+            pygame.event.get()
         pygame.display.flip()
         clock.tick(FPS)
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('start.png'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image(START_IMG_PATH), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    # for line in intro_text:
-    #     string_rendered = font.render(line, 1, colors.WHITE)
-    #     intro_rect = string_rendered.get_rect()
-    #     text_coord += 10
-    #     intro_rect.top = text_coord
-    #     intro_rect.x = 10
-    #     text_coord += intro_rect.height
-    #     screen.blit(string_rendered, intro_rect)
+    animation_surface = pygame.Surface((60, 420))
 
+    competitive_mode_button = pygame.rect.Rect(332, 113, 267, 158)
+    free_mode_button = pygame.rect.Rect(334, 382, 158, 104)
+
+    text_coord = 220
+    records_font_size = 80
+
+    for record in records_read():
+        records_font = pygame.font.Font(PIXEL_FONT_PATH, records_font_size)
+        string_rendered = records_font.render(str(record), 1, colors.WHITE)
+        intro_rect = string_rendered.get_rect()
+        intro_rect.top = text_coord
+        intro_rect.x = 713 + 76 - intro_rect.width / 2
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+        records_font_size -= 10
+
+    last_animation_time = time.time()
     while True:
+        if time.time() - last_animation_time > 0.5:
+            animation_surface.fill(colors.BLACK)
+            animation_sprites.update()
+            screen.blit(animation_surface, (194, 90))
+            animation_sprites.draw(screen)
+            last_animation_time = time.time()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == pygame.BUTTON_LEFT:
-                    if pygame.rect.Rect(380, 265, 240, 70).collidepoint(event.pos):
+                    if competitive_mode_button.collidepoint(event.pos):
                         return 1
-                    elif pygame.rect.Rect(380, 393, 240, 70).collidepoint(event.pos):
+                    elif free_mode_button.collidepoint(event.pos):
                         return 2
+
         pygame.display.flip()
         clock.tick(FPS)
 
 
-# Размер клетки по умолчанию
-CELL_SIZE = 30
+class AnimatedSprite(pygame.sprite.Sprite):
+
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(animation_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
 
 
 class Segment(pygame.sprite.Sprite):
+
     def __init__(self, sprite_group, pos_x, pos_y, color, scale=1.0):
         super().__init__(sprite_group)
         self.scale = scale
@@ -120,7 +182,8 @@ class Segment(pygame.sprite.Sprite):
 
 
 class Shape(pygame.sprite.Group):
-    def __init__(self, shape, color, scale=1.0):
+
+    def __init__(self, shape, color, scale=1):
         super().__init__()
         self.shape = shape
         self.color = color
@@ -156,21 +219,24 @@ class Shape(pygame.sprite.Group):
 
 
 class Field(Surface):
+
     def __init__(self, x, y, width, height):
         self.x, self.y = x, y
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-        self.cell_size = cell_size
+        self.cell_size = CELL_SIZE
 
         self.cells = pygame.sprite.Group()
         self.next_shape = self.get_random_shape()
+        self.next_shape.move(3, 0)
         self.shape: Shape | None = None
         super().__init__((width * self.cell_size, height * self.cell_size))
 
         self.last_fall_time = time.time()
         self.tick = 0.7
         self.score = 0
+        self.timer = time.time()
         self.run = False
 
     def draw(self, surface):
@@ -210,7 +276,6 @@ class Field(Surface):
 
                     if self.shape.y == 0:
                         self.run = False
-                        return self.score
 
                     self.cells.add(self.shape.sprites())
                     self.new_shape()
@@ -231,22 +296,21 @@ class Field(Surface):
                         if cell.y < row:
                             cell.move(0, 1)
 
-
         else:
             self.run = False
-
-        return self.score
 
     def start(self):
         self.run = True
         self.new_shape()
 
     def get_random_shape(self):
-        return Shape(tetris_shapes[random.choice(list(tetris_shapes.keys()))], colors.WHITE)
+        shape = Shape(tetris_shapes[random.choice(list(tetris_shapes.keys()))], colors.WHITE)
+        return shape
 
     def new_shape(self):
         self.shape = self.next_shape
         self.next_shape = self.get_random_shape()
+        self.next_shape.move(3, 0)
 
     def action_left(self):
         self.shape.move(-1, 0)
@@ -270,6 +334,14 @@ class Field(Surface):
                 return
         self.shape = new_shape
 
+    def drop_shape(self):
+        while True:
+            self.shape.move(0, 1)
+
+            if self.is_shape_collide(self.shape):
+                self.shape.move(0, -1)
+                break
+
     def is_shape_collide(self, shape):
         other_cells = self.cells.copy()
         for cell in shape:
@@ -283,35 +355,80 @@ class Field(Surface):
     def set_tick(self, tick):
         self.tick = tick
 
+    def end_game(self):
+        self.run = False
+
 
 clock = pygame.time.Clock()
 
-f1 = pygame.font.Font(None, 80)
+f1 = pygame.font.Font(PIXEL_FONT_PATH, 80)
+
+animation = AnimatedSprite(load_image(ANIMATION_PATH), 17, 1, 194, 90)
 
 while True:
     mode = start_screen()
 
     game_running = True
 
-    field = Field(WIDTH // 2 - (field_width // 2 * cell_size), - 2 * cell_size, field_width, field_height + 2)
+    field = Field(WIDTH // 2 - (field_width // 2 * CELL_SIZE), - 2 * CELL_SIZE, field_width, field_height + 2)
 
     field.start()
 
     while game_running:
         screen.fill(colors.BLACK)
 
-        score = field.update()
+        field.update()
         field.draw(screen)
-        text1 = f1.render(str(score), True, colors.WHITE)
-        screen.blit(text1, (20, 15))
+        score = field.score
+        score_text = f1.render(str(score), True, colors.WHITE)
+        screen.blit(score_text, (((WIDTH - field_width * CELL_SIZE) / 2 - score_text.get_width()) / 2, HEIGHT * 0.3))
+
+        tick = 0.7
+        if mode == 1:
+            time_left = 180 - time.time() + field.timer
+            time_left_text = f1.render(f"{int(time_left // 60)}:{int(time_left % 60):02}", True, colors.WHITE)
+            screen.blit(time_left_text,
+                        (((WIDTH - field_width * CELL_SIZE) / 2 - time_left_text.get_width()) / 2, HEIGHT * 0.15))
+
+            # Уровни сложности
+            if time_left <= 30:
+                level_text = f1.render('LEVEL 6', True, colors.WHITE)
+                tick = 0.2
+            elif time_left <= 60:
+                level_text = f1.render('LEVEL 5', True, colors.WHITE)
+                tick = 0.3
+            elif time_left <= 90:
+                level_text = f1.render('LEVEL 4', True, colors.WHITE)
+                tick = 0.4
+            elif time_left <= 120:
+                level_text = f1.render('LEVEL 3', True, colors.WHITE)
+                tick = 0.5
+            elif time_left <= 150:
+                level_text = f1.render('LEVEL 2', True, colors.WHITE)
+                tick = 0.6
+            else:
+                level_text = f1.render('LEVEL 1', True, colors.WHITE)
+                tick = 0.7
+            screen.blit(level_text, (((WIDTH - field_width * CELL_SIZE) / 2 - level_text.get_width()) / 2, 0))
+
+            if time_left <= 0:
+                game_running = False
 
         pygame.draw.rect(screen, colors.WHITE, (WIDTH / 3 * 2 + (WIDTH / 3 - 150) / 2, HEIGHT / 2 - 150 / 2, 150, 150),
                          width=1)
         shape_preview = Shape(field.next_shape.shape, field.next_shape.color)
-        shape_preview.move(WIDTH / 3 * 2 + (WIDTH / 3 - 150) / 2 + 30, HEIGHT / 2 - 150 / 2 + 30, cell_based=False)
+        shape_preview.move(WIDTH / 3 * 2 + (WIDTH / 3 - 150) / 2 + (
+                150 - (max([i.rect.x for i in shape_preview.sprites()]) + CELL_SIZE)) / 2, HEIGHT / 2 - 150 / 2 + (
+                                   150 - (max([i.rect.y for i in shape_preview.sprites()]) + CELL_SIZE)) / 2,
+                           cell_based=False)
         shape_preview.draw(screen)
 
-        menu_button = pygame.draw.rect(screen, colors.WHITE, (WIDTH / 3 / 4, HEIGHT * 0.85, WIDTH / 3 / 2, 50))
+        menu_button = pygame.draw.rect(screen, colors.WHITE,
+                                       (((WIDTH - field_width * CELL_SIZE) / 2 - 200) / 2, HEIGHT * 0.85, 200, 50))
+        menu_text = f1.render('quit', True, colors.BLACK)
+        screen.blit(menu_text,
+                    (((WIDTH - field_width * CELL_SIZE) / 2 - 200) / 2 + ((200 - menu_text.get_width()) / 2),
+                     HEIGHT * 0.85 - menu_text.get_height() * 0.25))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -325,20 +442,24 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
                 field.set_tick(0.05)
             if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
-                field.set_tick(0.7)
+                field.set_tick(tick)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                field.drop_shape()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == pygame.BUTTON_LEFT:
-                    print(pygame.rect.Rect(380, 265, 240, 70).collidepoint(event.pos))
                     if menu_button.collidepoint(event.pos):
+                        if mode == 1:
+                            records_add(score)
                         game_running = False
 
+        if field.tick != 0.05:
+            field.set_tick(tick)
+
         if not field.run:
-            if game_over_screen(score):
-                field = Field(WIDTH // 2 - (field_width // 2 * cell_size), - 2 * cell_size, field_width,
-                              field_height + 2)
-                field.start()
-            else:
-                terminate()
+            game_running = False
 
         pygame.display.flip()
         clock.tick(FPS)
+
+    if mode == 1:
+        game_over_screen(score)
--- pygame_utils.py
+++ pygame_utils.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import os
 
+
 def terminate():
     pygame.quit()
     sys.exit()
@@ -9,6 +10,7 @@ def terminate():
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
+
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -22,18 +24,3 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
-
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
